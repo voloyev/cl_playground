496130159782853969f7b1333b294b033f634369
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-06-25 02:01:22 +0300

    Init

---
+++ db.lisp
@@ -0,0 +1,24 @@
+(load "~/quicklisp/setup.lisp")
+
+(defvar *db* nil)
+
+(defun make-cd (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+(defun add-record (cd) (push cd *db*))
+
+(defun dunp-db ()
+  (dolist (cd *db*)
+    (format t "~{~a:~10t~a~%~}~%" cd)))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-ior*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (prompt-read "Rating")
+   (prompt-read "Ripped[y/n]")))
