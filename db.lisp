665d34582e473eef24815ed11338aad4259ebdfc
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-05-11 14:37:41 +0300

    Add new version

--- db.lisp
+++ db.lisp
@@ -21,4 +21,49 @@
    (prompt-read "Title")
    (prompt-read "Artist")
    (prompt-read "Rating")
-   (prompt-read "Ripped[y/n]")))
+   (prompt-read "Ripped[y/n]: ")))
+
+(defun add-cds ()
+    (loop (add-record (prompt-for-cd))
+       (if (not (y-or-n-p "Another? [y/n]"))
+           (return))))
+
+(defun save-db (filename)
+  (with-open-file (out filename
+                       :direction :output
+                       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
+
+(defun artist-selector (artist)
+  #'(lambda (cd) (equal (getf cd :artist) artist)))
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  #'(lambda (cd)
+      (and
+       (if title (equal (getf cd :title) title) t)
+       (if artist (equal (getf cd :artist) artist) t)
+       (if rating (equal (getf cd :rating) rating) t)
+       (if ripped-p (equal (getf cd :ripped) ripped) t))))
+
+(defun update (selector-fn &key title artist rating (ripped nil ripped-p))
+  (setf *db*
+        (mapcar
+         #'(lambda (row)
+             (when (funcall selector-fn row)
+               (if title (setf (getf row :title) title))
+               (if artist (setf (getf row :artist) artist))
+               (if rating (setf (getf row :rating) rating))
+               (if ripped-p (setf (getf row :ripped) ripped)))
+             row) *db*)))
+
+(defun delete-rows (selector-fn)
+  (setf *db* (remove-if selector-fn *db*)))
